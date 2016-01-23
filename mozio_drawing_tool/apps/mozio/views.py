4333c0f57d5c6e287dcb49ebb9f869bcd8c51d1e
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-23 19:15:46 -0200

    Update index view in order to retrieve the latest data inserted

--- mozio_drawing_tool/apps/mozio/views.py
+++ mozio_drawing_tool/apps/mozio/views.py
@@ -5,24 +5,34 @@ from django.utils.translation import ugettext_lazy as _
 from django.template import RequestContext
 
 from .forms import CreateServiceAreaForm
+from .models import ServiceAreaCompany
 
 
 def index(request):
+    latest_service_area = ServiceAreaCompany.objects.select_related().order_by('-created_at')[0]
+
     context = {
         'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY,
-        'form': CreateServiceAreaForm
+        'form': CreateServiceAreaForm,
+        'latest_service_area': latest_service_area
     }
 
-    return render_to_response('mozio/index.html', RequestContext(request, context))
+    return render_to_response('mozio/index.html', context,
+                              context_instance=RequestContext(request))
 
 def create_service_area(request):
     if request.method == 'POST':
         form = CreateServiceAreaForm(request.POST)
         if form.is_valid():
+            # points must be sent like a list
             points = request.POST.getlist('latitude_and_longitude[]')
-            form.save(points)
-            messages.success(request, _('Success! Your Service Area was saved!'))
+
+            if len(points) > 0:
+                form.save(points)
+                messages.success(request, _('Success! Your Service Area was saved!'))
+            else:
+                messages.error(request, _('No points were provided.'))
         else:
-            messages.error(request, _('Invalid form: %s' % form.errors))
+            messages.error(request, _(form.get_error_messages()))
 
         return redirect('mozio_index')
