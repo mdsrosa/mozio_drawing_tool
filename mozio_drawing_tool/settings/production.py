f3ca5300f635970eb23517006a6abc24d310ec12
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-23 20:25:34 -0200

    Add production settings for AWS

--- mozio_drawing_tool/settings/production.py
+++ mozio_drawing_tool/settings/production.py
@@ -2,6 +2,17 @@ from mozio_drawing_tool.settings.base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mozio',
+        'USER': 'mozio',
+        'PASSWORD': 'moziodjangoapp'
+        'HOST': 'mozio-django-app-mysql-db.ckcjy5xii4eu.sa-east-1.rds.amazonaws.com',
+        'PORT': '3306'
+    }
+}
+
 # Use the cached template loader so template is compiled once and read from
 # memory instead of reading from disk on each load.
 TEMPLATES[0]['OPTIONS']['loaders'] = [
@@ -9,3 +20,10 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
         ['django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader'])
 ]
+
+INSTALLED_APPS += ('storages',)
+
+AWS_STORAGE_BUCKET_NAME = 'mozio-django-app-staticfiles'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
