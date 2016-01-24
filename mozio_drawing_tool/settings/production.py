0cb02a92cd8187a3ba756c094fb629a47158d6d7
Matheus Rosa <matheusdsrosa@gmail.com>
2016-01-23 22:16:06 -0200

    Set APP_DIRS to False in production settings

--- mozio_drawing_tool/settings/production.py
+++ mozio_drawing_tool/settings/production.py
@@ -15,6 +15,7 @@ DATABASES = {
 
 # Use the cached template loader so template is compiled once and read from
 # memory instead of reading from disk on each load.
+TEMPLATES[0]['APP_DIRS'] = False
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     ('django.template.loaders.cached.Loader',
         ['django.template.loaders.filesystem.Loader',
